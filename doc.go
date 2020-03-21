13d4c8625da2e782bc4a8d5db5bedf6be6c4ae5b
slhamptonii <s.l.hamptonii@gmail.com>
2020-03-20 19:51:29 -0700

    added initial doc.go and so.sum

---
+++ doc.go
@@ -0,0 +1,7 @@
+// The toolbelt package is the common library for work.
+// It currently has the following sub-modules:
+// * l
+//   * A tool for standardizing logs into a JSON format
+// * constants
+//   * A tool for supplying common string; variables, formats, regular expressions, etc.
+package toolbelt
