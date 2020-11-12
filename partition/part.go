17546c927f0ac5aa512d9d96c7bc5b40cdec036a
slhamptonii <s.l.hamptonii@gmail.com>
2020-11-11 16:43:59 -0800

    removed unnecessary steps in partition

--- partition/part.go
+++ partition/part.go
@@ -6,10 +6,7 @@ import (
 )
 
 type IntPartition interface {
-	Next()
-	Before([]int)
 	Process([]int)
-	After([]int)
 	Do([]int)
 }
 
@@ -25,50 +22,32 @@ func (p *PercentPartition) Next() {
 	p.Right = p.Length * p.Segment / p.Partitions
 }
 
-func (p *PercentPartition) Before(them []int) {
-}
-
 func (p *PercentPartition) Process(them []int) {
 	fmt.Println(them)
-}
-
-func (p *PercentPartition) After(them []int) {
 }
 
 func (p *PercentPartition) Do(them []int) {
 	p.Next()
 	for p.Right != p.Length {
-		p.Before(them[p.Left:p.Right])
 		p.Process(them[p.Left:p.Right])
-		p.After(them[p.Left:p.Right])
 		p.Left = p.Right
 		p.Segment ++
 		p.Next()
 	}
-	p.Before(them[p.Left:p.Right])
 	p.Process(them[p.Left:p.Right])
-	p.After(them[p.Left:p.Right])
 }
 
 type SimplePartition struct {
 	Partitions int
 }
 
-func (p *SimplePartition) Next() {}
-
-func (p *SimplePartition) Before(them []int) {
-}
 func (p *SimplePartition) Process(them []int)  {
 	fmt.Println(them)
-}
-func (p *SimplePartition) After(them []int)  {
 }
 
 func (p *SimplePartition) Do(them []int) {
 	for idxRange := range gopart.Partition(len(them), p.Partitions) {
 		segment := them[idxRange.Low:idxRange.High]
-		p.Before(segment)
 		p.Process(segment)
-		p.After(segment)
 	}
 }
